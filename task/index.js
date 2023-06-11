0fe75daed5baca3a64a35e1af68b311efad8a024
Hubertoos <Hubertoos@users.noreply.github.com>
2023-06-11 13:35:30 +0200

    Zadanie findPhraseInArray
    Pierwszy commit zadania findPhraseInArray

---
+++ task/index.js
@@ -0,0 +1,18 @@
+import x from './example';
+import variableFromExample from './example'
+
+function main() {
+    const wordsArr = ["pierwszy","drugi","trzeci", "domek", "autostrada", 
+    "prestidigitator","coraz","wiecej","nowych",'wyrazów','czacha','dymi','mózg','paruje','szuflandia'];
+
+    const findPhraseInArray = (array, phrase) => {
+        return array.reduce((acc, elem, index) => {
+            if (elem.includes(phrase)) acc.push([elem, index])
+            return acc;
+        },[]);
+      };
+      console.log(findPhraseInArray(wordsArr,'u'))
+
+}
+
+export default main
